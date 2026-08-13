f05cb03bb48684a845961c0b891cacc0cbecb505
Pritom099 <pritompaul515@gmail.com>
2026-08-13 17:02:01 +0600

    second commit

--- src/app/(withCommonLayout)/services/page.jsx
+++ src/app/(withCommonLayout)/services/page.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import ServiceCard from './_component/ServiceCard';
 
-const servicesPage = () => {
+const getAllServices = async() => {
+    const res = await fetch("https://car-washing-system-cleanify-server.vercel.app/api/v1/services");
+    const data = await res.json();
+    return data;
+}
+
+const servicesPage = async() => {
+    const services  = await getAllServices();
+    console.log(services);
+
     return (
         <div>
             <h2>services  Page</h2>
+
+            {services?.data?.map((service) => {
+                return <ServiceCard service={service} key={service?._id}></ServiceCard>;
+            })}
         </div>
     );
 };
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -2,25 +2,30 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import logo from "@/assets/img/logo.jpg"
+import Container from './Container';
 
 const Navbar = () => {
     return (
-        <div className='h-[150px] bg-purple-200 text-black  font-bold text-5xl flex justify-center items-center'>
+        <div className='bg-purple-500'>
+            <Container>
+                <div className='py-4 text-black font-bold text-5xl flex justify-between items-center'>
 
-            <Link href={"/"}>
-                <Image src={logo} alt="logo" height="60" width="60"></Image>
-            </Link>
-            
-            <ul>
-                <Link href={"/"}>Home</Link>
-                <Link href={"/services"}>Services</Link>
-                <Link href={"/reviews"}>Reviews</Link>
-                <Link href={"/aboutUs"}>About Us</Link>
-            </ul>
+                    <Link href={"/"}>
+                        <Image src={logo} alt="logo" height="60" width="60"></Image>
+                    </Link>
 
-            <Link href={"/login"}>
-            <button>Login</button>
-            </Link>
+                    <ul className='font-semibold text-[18px] flex gap-3 items-center'>
+                        <Link href={"/"}>Home</Link>
+                        <Link href={"/services"}>Services</Link>
+                        <Link href={"/reviews"}>Reviews</Link>
+                        <Link href={"/aboutUs"}>About Us</Link>
+                    </ul>
+
+                    <Link href={"/login"}>
+                        <button>Login</button>
+                    </Link>
+                </div>
+            </Container>
         </div>
     );
 };
